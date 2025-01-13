f9dc7b2928418066f95605acc57e2e751dbfc749
proshaban <shaban.khan.self@gmail.com>
2025-01-13 23:10:42 +0530

    First Commit

---
+++ BinaryTree/BinaryTree.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+//build tree using preorder sequence 
+
+
+int main() {
+    int a;
+    cout << "Hello, World!" << endl;
+    cin>>a;
+    cout<<"You have entered: "<<a;
+    return 0;
+}
