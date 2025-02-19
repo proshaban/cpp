0b2f564fcd2e3674d9a310b20d81b0e9645d749f
proshaban <shaban.khan.self@gmail.com>
2025-02-19 23:59:53 +0530

    Trnasform

--- BinaryTree/BinaryTree.cpp
+++ BinaryTree/BinaryTree.cpp
@@ -492,6 +492,29 @@ int KthAnc(Node* root, int node, int k)
     return validVal+1;
 }
 
+/*
+=======================================================================================================
+10. Transform to Sum Tree
+=======================================================================================================
+*/
+
+int transform(Node* root)
+{
+    if(root==NULL)  return 0;
+
+    int leftOld = transform(root->left);
+    int rightOld = transform(root->right);
+    int currOld = root->data;
+
+    root->data = leftOld+rightOld;
+
+    if(root->left !=NULL) root->data += root->left->data;
+    if(root->right !=NULL) root->data += root->right->data;
+
+    return currOld
+
+}
+
 int main()
 {
 
