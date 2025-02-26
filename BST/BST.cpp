c6974c473d80991e2c44e184c86f51d8fbf47180
proshaban <shaban.khan.self@gmail.com>
2025-02-26 12:04:49 +0530

    Delete a node from BST

--- BST/BST.cpp
+++ BST/BST.cpp
@@ -87,6 +87,62 @@ bool search(Node* root, int key)
     else return search(root->left , key);
 }
 
+
+/*
+=======================================================================================================
+3. Inorder Sucessor of a node. It's the left most node on right subtree
+======================================================================================================
+*/
+
+Node* getInOrderSuccessor(Node* root)
+{
+    while (root->left != NULL)
+    {
+        root = root->left;
+    }
+    return root;
+}
+
+Node* delNode(Node* root, int val)
+{
+    if(root == NULL) return NULL;
+
+    if(val < root->data )
+    {
+        root->left = delNode(root->left , val);
+    }
+    else if( val > root->data)
+    {
+        root->right = delNode(root->right , val);
+    }
+    else {
+        //root = val
+
+        //case 1: 0 children
+        if(root->left == NULL && root->right ==NULL) 
+        {
+            delete root;
+            return NULL;
+        }
+
+        //case 2: 1 Child
+        if(root->left == NULL || root->right == NULL)
+        {
+            return root->left == NULL ? root->right : root->left;
+        }
+
+        //case 3: 2 children 
+
+        Node * IS = getInOrderSuccessor(root->right);
+        root->data = IS->data;
+        root->right = delNode(root->right, IS->data);
+        return root;
+    }
+
+    return root;
+}
+
+
 int main()
 {
     // arr[6]= {5, 1, 3, 4, 2, 7};
@@ -94,6 +150,10 @@ int main()
     int key = 6;
     Node* root = buildBST(arr2,9);
     
-    cout<<"is key "<<6<<"presented in BST:"<<search(root,key)<<" ";
+    inorder(root);
+    cout<<endl;
+
+    delNode(root,6);
+    inorder(root);
     cout<<endl;
 }
