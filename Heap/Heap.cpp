0396e4e3afda5db6901453a6921e037a2f2aefd2
proshaban <shaban.khan.self@gmail.com>
2025-03-06 13:00:57 +0530

    Heap Push

--- Heap/Heap.cpp
+++ Heap/Heap.cpp
@@ -2,21 +2,48 @@
 using namespace std;
 /*
 =======================================================================================================
-1. We are using Priority queue for this 
-
- priority_queue<int>pq;
-
- -push()
- -pop()
- -top()
+1. creating Heap
 ======================================================================================================
 */
+class Heap{
+    vector<int>v;
+    public:
+    void push(int val)
+    {
+        v.push_back(val);
+        int x = v.size()-1;
+        int parent = (x-1)/2;
+        while(parent>=0 && v[parent]<v[x])
+        {
+            swap(v[parent],v[x]);
+            x = parent;
+            parent = (x-1)/2;
+        }
+    }
+
+    void pop()
+    {
+
+    }
+
+    int top()
+    {
+        return v[0];
+    }
+
+    int empty()
+    {
+        return v.size()==0;
+    }
+
+};
+
 int main()
 {
-    priority_queue<int>pq;
-    pq.push(5);
-    pq.push(10);
-    pq.push(3);
-    cout<<"Top of pq:"<<pq.top()<<endl;
+    Heap heap;
+    heap.push(5);
+    heap.push(15);
+    heap.push(25);
+    cout<<"Top of heap:"<<heap.top()<<endl;
     return 0;
 }
